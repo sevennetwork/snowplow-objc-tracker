56b6dfa40d6e88fc6a6b671d635659ab6d062879
Alex Benini <alex.benini@snowplowanalytics.com>
2020-05-04 12:06:16 +0200

    Test multiple osx_image environment

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -13,6 +13,7 @@ fi
 
 printf "\n\n Setup env variables \n"
 
+BUILD_DEST_OLD_IOS="platform=iOS Simulator,OS:8.1,name:iPad 2"
 BUILD_DEST_IOS="platform=iOS Simulator,OS=13.4,name=iPhone 11 Pro"
 BUILD_DEST_PAIRED="platform=iOS Simulator,OS=13.4,name=iPhone 11 Pro"
 BUILD_DEST_WATCH="platform=watchOS Simulator,OS=6.2,name=Apple Watch Series 5 - 44mm"
